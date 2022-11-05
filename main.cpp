71ccaa29b0e5d7da2b2f9f7f7e2b5bdf5147271c
wdy0311 <54197557+wdy0311@users.noreply.github.com>
2022-11-05 12:49:28 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  3.2
+//
+//  Created by 王东阳 on 2022/10/30.
+//
+
+#include <iostream>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    string a;
+    //cin>>a;
+    vector<string> v1;
+    while(cin>>a)
+    {
+        v1.push_back(a);
+    }
+    return 0;
+}
